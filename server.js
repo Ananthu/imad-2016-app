e6a95568a4054681650e11d70e0b857b19b7db48
Ananthu Saseendran <begoodananthu@gmail.com>
2016-10-13 17:27:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req,res) {
-  res.send('this is article one');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one'));
 });
 
 app.get('/article-Two', function(req,res) {
